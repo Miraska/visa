dc198ffe68712714bb25044defc05f1109bc1560
Miraska <102532370+Miraska@users.noreply.github.com>
2022-03-29 01:37:14 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$phone = $_POST['tel'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'wisaptest@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'u87CJcG8MYra1enpASCX'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('wisaptest@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('miraska.007@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка на приобритение визы с сайта VISA PARADISE';
+$mail->Body    = ' Заказчик ' .$name . ' оставил заявку на услугу "' .$country. '" <br>Телефон заказчика: ' .$phone. '<br>Почта заказчика: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: index.html');
+}
+?>
